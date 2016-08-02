a1e64450bbebef62e5449512d96283905a853440
Chris Combs <ccombs@gmail.com>
2016-08-01 22:53:47 -0400

    autorestart

--- trello-screen.sh
+++ trello-screen.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 cd /home/chip
 /usr/sbin/i2cset -f -y 0 0x34 0x93 0x0
-python trello-screen.py
+
+until python trello-screen.py; do 
+    echo "'trello-screen.py' crashed with exit code $?.  Respawning.." >&2
+    sleep 1
+done
+
