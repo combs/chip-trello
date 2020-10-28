6e3ab524d4348431ee7f344ea3cb4b2e02686ae0
Chris Combs <ccombs@gmail.com>
2020-10-27 20:31:27 -0400

    switch to python3

--- trello-screen.py
+++ trello-screen.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #
 # !!! Needs psutil (+ dependencies) installing:
 #
--- trello-screen.sh
+++ trello-screen.sh
@@ -2,7 +2,7 @@
 cd /home/chip
 /usr/sbin/i2cset -f -y 0 0x34 0x93 0x0
 
-until python trello-screen.py; do 
+until python3 trello-screen.py; do 
     echo "'trello-screen.py' crashed with exit code $?.  Respawning.." >&2
     sleep 1
 done
